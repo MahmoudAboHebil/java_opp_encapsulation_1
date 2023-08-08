6834e78b0c9c30f4bce43f7198bbcfd0e8a5d668
Mahmoud <mahmoudabohebil@gmail.com>
2023-08-08 16:07:05 +0300

    Creating the Getters method.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        Car car =new  Car();
+        car.describeCar();
+        System.out.println("make = "+ car.getMake());
+        System.out.println("model = "+ car.getModel());
     }
 }
